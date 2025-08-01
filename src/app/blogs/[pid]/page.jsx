f9a53f0cc71190ebd8806c19bc4b295ee8f5bc09
HamizMuzaffer <hamiz.muzaffer77@gmail.com>
2025-08-01 14:36:17 +0500

    changes added

--- src/app/blogs/[pid]/page.jsx
+++ src/app/blogs/[pid]/page.jsx
@@ -17,7 +17,7 @@ const BlogPost = () => {
   useEffect(() => {
     const fetchComments = async () => {
       try {
-        const res = await fetch(`/api/comment?blogId=${pid}`);
+        const res = await fetch(`https://www.hamizmuzaffer.me/api/comment?blogId=${pid}`);
         const data = await res.json();
         setComments(data);
       } catch (err) {
